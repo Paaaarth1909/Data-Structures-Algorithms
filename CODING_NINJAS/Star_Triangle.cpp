6f538c299b6b5f4fd806327db38eb564b2fe83a1
Parthsaarthie sharma <toshit113@gmail.com>
2025-07-30 21:42:24 +0530

    COMMIT

---
+++ CODING_NINJAS/Star_Triangle.cpp
@@ -0,0 +1,13 @@
+/* Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+  *
+ ***
+*****
+*/
+
