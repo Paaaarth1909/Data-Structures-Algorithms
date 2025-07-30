ad716b8cb85e4fff401e3f5bf68efa9668dfb779
Parthsaarthie sharma <toshit113@gmail.com>
2025-07-30 21:22:33 +0530

    COMMIT

--- CODING_NINJAS/seeding.cpp
+++ CODING_NINJAS/seeding.cpp
@@ -9,4 +9,22 @@ Output:
 * * *
 * *
 * */
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    seeding(n);
+    return 0;
+}
 
+void seeding(int n) {
+
+    for (int i = n; i >= 1; i--) { // Loop for each row
+        for (int j = 1; j <= i; j++) { // Loop for printing stars in each row
+            cout << "* "; // Print star
+        }
+        cout << endl; // Move to the next line after printing stars in a row
+    }
+}
