0adf0ebdc844770352513a1cc48f56b0b516ab47
Parthsaarthie sharma <toshit113@gmail.com>
2025-07-30 20:38:17 +0530

    COMMIT

---
+++ CODING_NINJAS/N_forest_problem.cpp
@@ -0,0 +1,13 @@
+/* Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+
+For every value of ‘N’, help sam to print the corresponding N-dimensional forest.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+* * *
+* * *
+* * *   */
+
+
