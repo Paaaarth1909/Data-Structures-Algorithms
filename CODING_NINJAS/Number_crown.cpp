7af740c618e671ecc5aec9bf832f3362a24abc76
Parthsaarthie sharma <toshit113@gmail.com>
2025-07-31 20:42:58 +0530

    COMMIT

---
+++ CODING_NINJAS/Number_crown.cpp
@@ -0,0 +1,14 @@
+/* Problem statement
+Aryan and his friends are very fond of the pattern. They want to make the Reverse N-Number Crown for a given integer' N'.
+
+Given 'N', print the corresponding pattern.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+1         1
+1 2     2 1
+1 2 3 3 2 1  */
+
