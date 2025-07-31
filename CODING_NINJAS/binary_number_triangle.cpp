4fac3648e796e287e6d3a7c1e9081eb66949de1a
Parthsaarthie sharma <toshit113@gmail.com>
2025-07-31 19:33:26 +0530

    COMMIT

---
+++ CODING_NINJAS/binary_number_triangle.cpp
@@ -0,0 +1,14 @@
+/* Problem statement
+Aryan and his friends are very fond of the pattern. For a given integer ‘N’, they want to make the N-Binary Number Triangle.
+
+You are required to print the pattern as shown in the examples below.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+1
+0 1
+1 0 1 */
+
