0cf3adb8afad8ebdae436e2f59c14a90ca52dfb0
Parthsaarthie sharma <toshit113@gmail.com>
2025-07-31 18:52:28 +0530

    COMMIT

---
+++ CODING_NINJAS/Reverse_star_triangle.cpp
@@ -0,0 +1,30 @@
+/* Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the Reverse N-Star Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+*****
+ ***
+  * */
+#include <iostream>
+using namespace std;    
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+  void nStarTriangle(int n) ;
+    nStarTriangle(n);
+    return 0;{
+for (int i = 1; i <= n; i++) { // Loop for each row
+        for (int j = 1; j < i; j++) { // Loop for leading spaces
+            cout << " "; // Print space
+        }
+        for (int k = 1; k <= (2 * (n - i) + 1); k++) { // Loop for printing stars
+            cout << "*"; // Print star
+        }
+        cout << endl; // Move to the next line after printing stars in a row
+    }
+}
