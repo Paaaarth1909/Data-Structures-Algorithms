6d006a2e1482f12eb30d36a6d8e8f85a6c40d189
Parthsaarthie sharma <toshit113@gmail.com>
2025-07-31 21:10:09 +0530

    COMMIT

--- CODING_NINJAS/increasing_letter_triangle.cpp
+++ CODING_NINJAS/increasing_letter_triangle.cpp
@@ -12,3 +12,15 @@ A B C */
 
 #include <iostream>
 using namespace std;
+
+void nLetterTriangle(int n) {
+for (int i = 1; i <= n; i++) { // Loop for each row
+        for (int j = 1; j <= i; j++) { // Loop for printing characters in each row
+            cout << char('A' + j - 1) << " "; // Print character
+        }
+        cout << endl; // Move to the next line after printing characters in a row
+    }
+    }
+
+    //Time Complexity: O(N^2)
+    //Space Complexity: O(1)
--- PATTERNS/pattern14.cpp
+++ PATTERNS/pattern14.cpp
@@ -14,7 +14,7 @@ int main() {
 
     for (int i = 1; i <= n; i++) { // Loop for each row
         for (int j = 1; j <= i; j++) { // Loop for printing characters in each row
-            cout << char('A' + j - 1); // Print character
+            cout << char('A' + j - 1) << " "; // Print character
         }
         cout << endl; // Move to the next line after printing characters in a row
     }
