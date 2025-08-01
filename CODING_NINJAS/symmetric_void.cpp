a6f6a9a38f9bed112120c1bdab7cac050b9f52e5
Parthsaarthie sharma <toshit113@gmail.com>
2025-08-01 22:01:04 +0530

    COMMIT

--- CODING_NINJAS/symmetric_void.cpp
+++ CODING_NINJAS/symmetric_void.cpp
@@ -16,3 +16,43 @@ Output:
 
 #include <iostream>
 using namespace std;
+void symmetry(int n) {
+    int iniS=0;
+    for (int i=0;i<n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<"*";
+
+        }
+        for (int j=0;j<iniS;j++){
+            cout<<" ";
+
+
+        }
+        for(int j=1;j<=n-i;j++){
+            cout<<"*";
+
+        }
+        iniS+=2;
+        cout<<endl;
+    }
+    iniS=8;
+    for (int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+
+        }
+        for (int j=0;j<iniS;j++){
+            cout<<" ";
+
+
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+
+        }
+        iniS-=2;
+        cout<<endl;
+    }
+}
+//Time Complexity: O(N^2)
+//Space Complexity: O(1)
