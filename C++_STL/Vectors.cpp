ccc976c1a5d258623ee73af276061f94d4394dc9
Parthsaarthie sharma <toshit113@gmail.com>
2025-08-02 22:48:12 +0530

    COMMIT

---
+++ C++_STL/Vectors.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void explainVector(){
+    vector<int>v;
+    v.push_back(1);
+    v.emplace_back(2);
+    vector<pair<int, int>> vec;
+    vec.push_back({1, 2});
+    vec.emplace_back(1, 2);
+    vector<int>v(5,100);
+    vector<int>v(5);
+    vector<int>v1(5,100);
+    vector<int>v2(v1);
+
+
+    
+}
