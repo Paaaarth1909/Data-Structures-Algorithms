04083dea0b528318518524d337e97e84e2adbcda
Parthsaarthie sharma <toshit113@gmail.com>
2025-08-03 00:02:12 +0530

    COmMIT

---
+++ C++_STL/Dequeue.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+#include<deque>
+void explainDeque(){
+    deque<int>dq;
+    dq.push_back(1); // {1}
+    dq.emplace_back(2); // {1,2}
+    dq.push_front(4); // {4,1,2}
+    dq.emplace_front(3); // {3,4,1,2}
+
+    dq.pop_back(); // {3,4,1}
+    dq.pop_front(); // {4,1}
+
+    dq.back(); // returns 1
+    dq.front(); // returns 4
+
+    // rest functions same as vector
+    // begin , end , rbegin , rend , clear , insert , size , swap
+
+
+
+}
