4229f011af11d8d6b3c1cdf07db2cafb3a904173
Parthsaarthie sharma <toshit113@gmail.com>
2025-08-06 23:59:15 +0530

    COMMIT

---
+++ BASIC MATHS REVISION/GCD.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int gcd(int a , int b){
+    while(a>0&&b>0){
+        if(a>b){
+            a=a%b;
+        }else{
+            b=b%a;
+        }
+    }
+    return a+b;
+}
