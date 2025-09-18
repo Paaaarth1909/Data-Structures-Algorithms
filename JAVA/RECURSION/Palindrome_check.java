d18461a50e2785186e63e36a661cea71694fcb40
Parthsaarthie sharma <toshit113@gmail.com>
2025-09-18 20:09:53 +0530

    COMMIT

---
+++ JAVA/RECURSION/Palindrome_check.java
@@ -0,0 +1,22 @@
+//WAP to check if a string is a palindrome using recursion
+import java.util.Scanner;
+
+public class Palindrome_check {
+    static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) return true;
+        if (str.charAt(start) != str.charAt(end)) return false;
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.next();
+        if (isPalindrome(str, 0, str.length() - 1)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+        sc.close();
+    }
+}
