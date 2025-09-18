97cc3e86140670ac5f4e71f46ec7812354c5ee77
Parthsaarthie sharma <toshit113@gmail.com>
2025-09-18 20:07:42 +0530

    COMMIT

---
+++ JAVA/RECURSION/Factorial_of_N_numbers.java
@@ -0,0 +1,21 @@
+// WAP to print facorial of N numbers 
+import java.util.Scanner;
+
+public class Factorial_of_N_numbers {
+    static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = sc.nextInt();
+        System.out.println("Factorial of " + n + " is: " + factorial(n));
+        sc.close();
+    }
+}
+
+
+// Time Complexity: O(n)
+// Space Complexity : O(n)
