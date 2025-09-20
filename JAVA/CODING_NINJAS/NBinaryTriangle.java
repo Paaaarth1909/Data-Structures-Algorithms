c362178b0d924bbdf926dcf8582a78ed0055d866
Parthsaarthie sharma <toshit113@gmail.com>
2025-09-20 12:48:55 +0530

    COMMIT

---
+++ JAVA/CODING_NINJAS/NBinaryTriangle.java
@@ -0,0 +1,35 @@
+/* Problem statement
+Aryan and his friends are very fond of the pattern. For a given integer ‘N’, they want to make the N-Binary Number Triangle.
+
+You are required to print the pattern as shown in the examples below.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+1
+0 1
+1 0 1 */
+public class NBinaryTriangle {
+    static void nBinaryTriangle(int n) {
+        int start;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) start = 1;
+            else start = 0;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(start + " ");
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        nBinaryTriangle(n);
+    }
+}
+
+//Time Complexity: O(N^2)   
+//Space Complexity: O(1)
