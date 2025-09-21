d1cb6b60d531feec7d3ad9498cdafbb66bf4af8e
Parthsaarthie sharma <toshit113@gmail.com>
2025-09-21 10:15:52 +0530

    COMMIT

---
+++ JAVA/CODING_NINJAS/increasing_number_triangle.java
@@ -0,0 +1,32 @@
+/* Problem statement
+Aryan and his friends are very fond of patterns. For a given integer ‘N’, they want to make the Increasing Number Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+1
+2 3
+4 5 6 */
+public class increasing_number_triangle {
+    static void nNumberTriangle(int n) {
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        nNumberTriangle(n);
+    }
+}
+
+
+    // Time Complexity: O(N^2)   
+    // Space Complexity: O(1)
