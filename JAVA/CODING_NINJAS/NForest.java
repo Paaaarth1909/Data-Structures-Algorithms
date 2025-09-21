37b865a8a8bb2dd849492646a9e865325caac103
Parthsaarthie sharma <toshit113@gmail.com>
2025-09-21 10:53:55 +0530

    COMMIT

---
+++ JAVA/CODING_NINJAS/NForest.java
@@ -0,0 +1,36 @@
+/* Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+
+For every value of ‘N’, help sam to print the corresponding N-dimensional forest.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+* * *
+* * *
+* * *   */
+
+import java.util.Scanner;
+
+public class NForest {
+    static void nForest(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        nForest(n);
+        sc.close();
+    }
+}
+
+
+//Time Complexity: O(N^2) - We have two nested loops, each running N times.
+//Space Complexity: O(1) - No additional space used apart from input and output.
