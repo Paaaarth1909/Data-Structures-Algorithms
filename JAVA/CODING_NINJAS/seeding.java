d8b72551f365898ef47e37af28afd210a3ec4609
Parthsaarthie sharma <toshit113@gmail.com>
2025-09-22 08:25:10 +0530

    COMMIT

---
+++ JAVA/CODING_NINJAS/seeding.java
@@ -0,0 +1,34 @@
+/* Sam is planting trees on the upper half region (separated by the left diagonal) of the square shared field.
+
+For every value of ‘N’, print the field if the trees are represented by ‘*’.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+* * *
+* *
+* */
+import java.util.Scanner;
+
+public class seeding {
+    public seeding(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        new seeding(n);
+        sc.close();
+    }
+}
+
+//Time Complexity: O(N^2)
+//Space Complexity: O(1)
