ebf341f57e1308728cc903ea2fbb670e260c55a9
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-03 14:25:54 +0530

    COMMIT

---
+++ JAVA/CODING_NINJAS/star_diamond.java
@@ -0,0 +1,51 @@
+/* Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Diamond.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+  *
+ ***
+*****
+*****
+ ***
+  * */
+import java.util.Scanner;
+
+public class star_diamond {
+    public static void nStarDiamond(int n) {
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        nStarDiamond(n);
+    }
+}
+
+
+    //Time Complexity: O(N^2)   
+    //Space Complexity: O(1)
