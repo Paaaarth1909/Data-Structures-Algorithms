9d0e13801887c43297395a0359ef935b7add87b4
Parthsaarthie sharma <toshit113@gmail.com>
2025-10-04 20:42:55 +0530

    COMMIT

---
+++ JAVA/PATTERNS/pattern7.java
@@ -0,0 +1,28 @@
+
+// WAP to print the following pattern:
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+
+import java.util.Scanner;
+
+public class pattern7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("Enter the number of rows: ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
